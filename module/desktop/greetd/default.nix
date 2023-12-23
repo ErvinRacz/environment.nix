152bd748aa0659a9d39fa55fbe8f471399b3a000
Ervin Racz <ervin.racz@protonmail.com>
2023-12-23 16:01:18 +0200

    test tui

--- module/desktop/greetd/default.nix
+++ module/desktop/greetd/default.nix
@@ -3,18 +3,22 @@
   # greetd session manager
   services.greetd = let
     session = {
-      command = "${lib.getExe config.programs.hyprland.package}";
+      # command = "${lib.getExe config.programs.hyprland.package}";
+      command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --time-format '%I:%M %p | %a • %h | %F' --cmd Hyprland";
       user = "${userConfig.username}" ;
     };
   in {
     enable = true;
     settings = {
-      terminal.vt = 1;
       default_session = session;
       initial_session = session;
     };
   };
 
+  environment.systemPackages = with pkgs; [
+    greetd.tuigreet
+  ];
+
   # unlock GPG keyring, which sotres SSH keys, stored credentials, etc on login
-  security.pam.services.greetd.enableGnomeKeyring = true;
+  # security.pam.services.greetd.enableGnomeKeyring = true;
 }
