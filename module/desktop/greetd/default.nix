f41ea57f8f7654084f64df20f384c0d72e3bf40c
Ervin Racz <ervin.racz@protonmail.com>
2023-12-23 16:19:29 +0200

    asdsad

--- module/desktop/greetd/default.nix
+++ module/desktop/greetd/default.nix
@@ -4,12 +4,13 @@
   services.greetd = let
     session = {
       # command = "${lib.getExe config.programs.hyprland.package}";
-      command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --time-format '%I:%M %p | %a • %h | %F' --cmd Hyprland";
+      command = "${pkgs.greetd.tuigreet}/bin/tuigreet --remember --user-menu --asterisks --time --time-format '%I:%M %p | %a • %h | %F' --cmd Hyprland";
       user = "${userConfig.username}" ;
     };
   in {
     enable = true;
     settings = {
+      terminal.vt = 1;
       default_session = session;
       initial_session = session;
     };
